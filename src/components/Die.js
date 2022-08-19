2b9f4621920cc957fabe65cc811660da7e88836b
Jason Trestiza <trestizajason@gmail.com>
2022-08-19 16:45:40 +0800

    update some css of the dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Die(props){
 
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "antiquewhite "
+        backgroundColor: props.isHeld ? "#59E391" : ""
         
     }
     
